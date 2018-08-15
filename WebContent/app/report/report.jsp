e6f213b33512bc37b07c4f1e84098275804716a6
Lambok Sianturi <lsianturi@gmail.com>
2018-08-15 17:24:51 +0700

    aju rpt

---
+++ WebContent/app/report/report.jsp
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" >
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Test IFrame</title>
+	</head>
+<body>
+ 
+<p>This is text.</p>
+ 
+<iframe src="http://localhost:8080/koperasi/ReportViewer?id=1" width="600" height="400">
+Your browser doesn't understand IFRAME.
+Download the PDF file <a href="http://itextdocs.lowagie.com/examples/com/lowagie/examples/general/HelloWorld.pdf">here</a> .
+</iframe>
+ 
+<p>Page End!</p>
+ 
+</body>
+</html>
--- src/com/benclaus/koperasi/report/ReportViewer.java
+++ src/com/benclaus/koperasi/report/ReportViewer.java
@@ -3,122 +3,124 @@
  */
 package com.benclaus.koperasi.report;
 
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
-import org.apache.struts.action.ActionError;
-import org.apache.struts.action.ActionErrors;
 
-import com.benclaus.koperasi.dao.trx.ConfigService;
-import com.benclaus.koperasi.utility.Constant;
+import com.benclaus.koperasi.action.trx.AjuAction;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JasperRunManager;
+import net.sf.jasperreports.engine.util.JRLoader;
 
 /**
- * @author Lambok 
+ * @author Lambok
  *
  */
 public class ReportViewer extends HttpServlet {
 	private static Logger log = Logger.getLogger(ReportViewer.class);
+
 	/**
 	 * 
 	 */
 	public ReportViewer() {
 		// TODO Auto-generated constructor stub
 	}
+
 	/**
-	* @see javax.servlet.http.HttpServlet#void (javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-	*/
+	 * @see javax.servlet.http.HttpServlet#void
+	 *      (javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse)
+	 */
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doPost(req, resp);
 	}
 
 	/**
-	* @see javax.servlet.http.HttpServlet#void (javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-	*/
+	 * @see javax.servlet.http.HttpServlet#void
+	 *      (javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse)
+	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		log.debug("Report Viewer");
-		int BUFSIZE = 4096;
-		ActionErrors errors = new ActionErrors();
-//		HttpSession session = request.getSession();
-//		RequestDispatcher rd = request.getRequestDispatcher("/invalidPage.jsp");
+		Integer kreditId = Integer.parseInt(request.getParameter("id"));
+		byte[] doc = null;//getAjuDocument(kreditId); // Method that generate and return PDF as byte[]
 
-//		Login userLogin = (Login) session.getAttribute(Constant.SES_USERLOGIN);
+		response.setContentType("application/pdf");
+		response.setHeader("Content-Disposition", "inline;filename=document.pdf"); // Change from "attachment" to
+																						// "inline" for opening in
+																						// browser (attachment should
+																						// download directly)
+		response.setContentLength(doc.length);
 
-		// check user logged in
-//		if (userLogin == null || !userLogin.isLoggedIn()) {
-//			log.debug("Invalid Login");
-//			errors.add(Constant.GLOBALERROR, new ActionError("error.invalidLogin"));
-//			request.setAttribute(Globals.ERROR_KEY, errors);
-//			rd.forward(request, response);
-//			return;
-//		}
-		
-		try{
-			ConfigService service = ConfigService.getInstance();
-			String type = request.getParameter("type");
-			Integer year = new Integer(request.getParameter("year"));
-			
-			/*AbsenSummary rpt = null;
-			if (type.equals("report")) {
-				Integer company = Integer.parseInt(request.getParameter("company"));
-				Integer empId = Integer.parseInt(request.getParameter("empId"));
-				Integer month = new Integer(request.getParameter("month"));
-				
-				rpt = service.getAttendanceReport(company, empId, month, year);
-			} else {
-				rpt = service.getSummaryReport(year);
-			}
-			
-			
-			OutputStream servletOutStream = response.getOutputStream();
-			log.debug("Write the report "+rpt.getFile()+" to browser..");
-			File file = new File(rpt.getFile());
-			if (file.isFile()) {
-				int length = 0;
-				ServletContext context  = getServletConfig().getServletContext();
-			    String mimetype = context.getMimeType( rpt.getFile() );
-				
-				response.setContentType( (mimetype != null) ? mimetype : "application/octet-stream" );
-				response.setContentLength( (int)file.length() );
-				response.setHeader( "Content-Disposition", "attachment; filename=\"" + file.getName() + "\"" );
-				
-				byte[] bbuf = new byte[BUFSIZE];
-				DataInputStream in = new DataInputStream(new FileInputStream(file));
-				
-				while ((in != null) && ((length = in.read(bbuf)) != -1)) {
-					servletOutStream.write(bbuf,0,length);
-				}
-				
-				in.close();
-				servletOutStream.flush();
-				servletOutStream.close();
-			} else {
-				errors.add(
-						Constant.GLOBALERROR,
-						new ActionError("error.downloadMsg"));
-			}*/
-			
-		}catch (Exception e) {
-			log.error(e.getMessage(), e);
-			errors.add(
-					Constant.GLOBALERROR,
-					new ActionError("error.exception", e.getMessage()));
+		ServletOutputStream out = null;
+		try {
+			out = response.getOutputStream();
+			out.write(doc);
+			out.flush();
+		} catch (IOException e) {
+		} finally {
+			if (out != null)
+				out.close();
 		}
+	}
 
-//		if (errors.size() > 0) {
-//			request.setAttribute(Globals.ERROR_KEY, errors);
-//			rd.forward(request, response);
-//			return;
-//		}
+	private void getAjuDocument(Integer kreditId){
+		Map<String, Object> param = new HashMap<>();
+		param.put("pt", "PT.GS");
+		param.put("aplikasi", "1. Ijazah SMP\n2. Jamsostek + BPJS\n3. ATM + Buku tabungan\n4. Parklaring PT. GS");
+		param.put("nik", "GS.1235");
+		param.put("nama", "SHOFYAH RAHMAN");
+		param.put("bagian", "AQ");
+		param.put("bank", "HANA BANK");
+		param.put("supervisor", "SAMY");
+		param.put("status", "RO");
+		param.put("surveyor", "ACEP");
+		param.put("ref_trf", "0");
+		param.put("hp", "0812-2345-8979 / 0813-8765-9876");
+		param.put("note", "0");
+		param.put("status_kerja", 0);
+		param.put("pokok", new Double(4000000));
+		param.put("cicilan", new Double(450000));
+		param.put("tenor", 10);
+		param.put("tgl_lunas", new Date());
+		param.put("no_kredit", "110.32459");
+		param.put("deadline", "9");
+		param.put("tgl_aju", new Date());
+
+		byte[] bytes = null;
+
+		try {
+//			JasperReport jasperReport =  (JasperReport)JRLoader.loadObject(
+//					AjuAction.class.getResourceAsStream("/com/benclaus/koperasi/resources/jrxml/Aju.jasper"));
+			
+			JasperReport jasperReport = JasperCompileManager.compileReport(
+					AjuAction.class.getResourceAsStream("/com/benclaus/koperasi/resources/jrxml/Aju.jrxml"));
+			JasperPrint printer = JasperFillManager.fillReport(jasperReport, param);
+//			bytes = JasperExportManager.exportReportToPdf(printer);
+			JasperExportManager.exportReportToPdfFile(printer, "d://test.pdf");
+			
+//			bytes = JasperRunManager.runReportToPdf(jasperReport, param);
+			
+		} catch (JRException e) {
+			e.printStackTrace();
+		}
+//		return bytes;
+	}
+	public static void main(String[] args) {
+		new ReportViewer().getAjuDocument(1);
 	}
 }
